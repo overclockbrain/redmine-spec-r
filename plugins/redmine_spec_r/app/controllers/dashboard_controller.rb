6ad89ee78726ddb25e4cf9c60de75cef933de5ea
hot brain <63366646+overclockbrain@users.noreply.github.com>
2026-01-14 16:39:44 +0900

    feat: MTR-4 サンプルのダッシュボード機能を追加し、関連するファイルを作成

---
+++ plugins/redmine_spec_r/app/controllers/dashboard_controller.rb
@@ -0,0 +1,8 @@
+class DashboardController < ApplicationController
+  unloadable
+
+  def index
+    # ここでデータを取得してViewに渡すんやけど、
+    # 今はとりあえず空っぽでOKや！
+  end
+end
--- plugins/redmine_spec_r/config/routes.rb
+++ plugins/redmine_spec_r/config/routes.rb
@@ -1,2 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+get 'spec_r/dashboard', to: 'dashboard#index'
